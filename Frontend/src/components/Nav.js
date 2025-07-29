3ceb1a9643a8bcafa575b07fc8802b58d02532ef
doubt-i <dolasnarendra310@gmail.com>
2025-07-29 11:52:05 +0530

    backend and Frontend files added

---
+++ Frontend/src/components/Nav.js
@@ -0,0 +1,14 @@
+  import React from 'react'
+  import './nav.css'
+  export default function Nav() {
+    return (
+      <div>
+        <nav class="navbar">
+       <div class="navbar-title">
+        <h1 style={{textAlign:"center",color:"white",marginLeft:"35px"}}>
+        WELCOME TO CDAC PUNE HOSTEL</h1>
+       </div>
+       </nav>
+    </div>
+    )
+  }
